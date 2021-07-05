76ccf21203ada6f7b8c1c46105406f55336de0bf
sohamvg <sohavmg11@gmail.com>
2021-07-05 14:12:29 +0530

    Add documentation for test files

--- entry/src/ohosTest/java/com/example/hassanusman/circulardialogs/CDialogOhosTest.java
+++ entry/src/ohosTest/java/com/example/hassanusman/circulardialogs/CDialogOhosTest.java
@@ -14,16 +14,25 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests for Circular Dialog.
+ */
 public class CDialogOhosTest {
     private final Context context = AbilityDelegatorRegistry.getAbilityDelegator().getAppContext();
     private final CDialog cDialog = new CDialog(context);
 
+    /**
+     * Test for bundle name.
+     */
     @Test
     public void testBundleName() {
         final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
         assertEquals("com.example.hassanusman.circulardialogs", actualBundleName);
     }
 
+    /**
+     * Test for setSize function.
+     */
     @Test
     public void testSize() {
         cDialog.setSize(500, 700);
@@ -31,12 +40,18 @@ public class CDialogOhosTest {
         assertEquals(700, cDialog.getHeight());
     }
 
+    /**
+     * Test for setContentText function.
+     */
     @Test
     public void testContentText() {
         cDialog.setContentText("Test");
         assertEquals("Test", cDialog.getMessageText().getText());
     }
 
+    /**
+     * Test for setTextSize function.
+     */
     @Test
     public void testTextSize() {
         cDialog.setTextSize(CDConstants.LARGE_TEXT_SIZE);
@@ -50,12 +65,18 @@ public class CDialogOhosTest {
         assertEquals(textSize, cDialog.getMessageText().getTextSize());
     }
 
+    /**
+     * Test for setAnimationDuration function.
+     */
     @Test
     public void testAnimationDuration() {
         cDialog.setAnimationDuration(2000);
         assertEquals(2000, cDialog.getAnimationDuration());
     }
 
+    /**
+     * Test for setBackgroundColor function.
+     */
     @Test
     public void testBackgroundColor() {
         RgbColor color = new RgbColor(255, 165, 0);
@@ -63,30 +84,45 @@ public class CDialogOhosTest {
         assertEquals(color, cDialog.getBackgroundColor());
     }
 
+    /**
+     * Test for setAnimationDuration function.
+     */
     @Test
     public void testAnimationSlide() {
         cDialog.setAnimation(CDConstants.SLIDE_FROM_LEFT_TO_RIGHT);
         assertEquals(CDConstants.SLIDE_FROM_LEFT_TO_RIGHT, cDialog.getAnimation());
     }
 
+    /**
+     * Test for setAnimationDuration function.
+     */
     @Test
     public void testAnimationSlide2() {
         cDialog.setAnimation(CDConstants.SLIDE_FROM_TOP_TO_BOTTOM);
         assertEquals(CDConstants.SLIDE_FROM_TOP_TO_BOTTOM, cDialog.getAnimation());
     }
 
+    /**
+     * Test for setAnimation function (SCALE).
+     */
     @Test
     public void testAnimationScale() {
         cDialog.setAnimation(CDConstants.SCALE_FROM_CENTER_TO_CENTER);
         assertEquals(CDConstants.SCALE_FROM_CENTER_TO_CENTER, cDialog.getAnimation());
     }
 
+    /**
+     * Test for setAnimation function (SLIDE).
+     */
     @Test
     public void testAnimationScale2() {
         cDialog.setAnimation(CDConstants.SCALE_FROM_RIGHT_TO_LEFT);
         assertEquals(CDConstants.SCALE_FROM_RIGHT_TO_LEFT, cDialog.getAnimation());
     }
 
+    /**
+     * Test for createAlert function.
+     */
     @Test
     public void testCreateAlertSize() {
         cDialog.createAlert("Test", CDConstants.SUCCESS, CDConstants.MEDIUM);
@@ -100,6 +136,9 @@ public class CDialogOhosTest {
         assertEquals(size, cDialog.getWidth());
     }
 
+    /**
+     * Test for createAlert function.
+     */
     @Test
     public void testCreateAlertBackground() {
         cDialog.createAlert("Test", CDConstants.SUCCESS, CDConstants.MEDIUM);
@@ -113,12 +152,18 @@ public class CDialogOhosTest {
         assertEquals(RgbColor.fromArgbInt(color), cDialog.getBackgroundColor());
     }
 
+    /**
+     * Test for createAlert function.
+     */
     @Test
     public void testCreateAlertImage() {
         cDialog.createAlert("Test", CDConstants.ERROR, CDConstants.MEDIUM);
         assertEquals(110, cDialog.getImage().getPixelMap().getImageInfo().size.height);
     }
 
+    /**
+     * Test for setAlertType function.
+     */
     @Test
     public void testSetAlertType() {
         cDialog.setAlertType(CDConstants.ERROR);
